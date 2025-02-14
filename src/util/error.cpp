7b3a284da312b297d79fb3e57c24d179cfb4b94f
Adin Scannell <amscanne@meta.com>
2025-02-14 14:35:05 -0800

    Add `ErrorOr` and associated types
    In order to avoid reinventing the wheel, these are imported directly
    from LLVM, with a header providing a layer of indirection.
    
    These errors must be checked and consumed in order to proceed, providing
    a layer of safety and enforcing good practices for error checking.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>

---
+++ src/util/error.cpp
@@ -0,0 +1,12 @@
+#include "util/error.h"
+
+namespace bpftrace {
+
+std::ostream& operator<<(std::ostream& out, const llvm::Error& err) {
+  llvm::raw_os_ostream raw_ostream(out);
+  raw_ostream << err;
+  raw_ostream.flush();
+  return out;
+}
+
+}  // namespace bpftrace
