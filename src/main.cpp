ff41e62abf5bd58ab97e7f8995f4e545b644c74e
Adin Scannell <amscanne@meta.com>
2025-06-13 16:11:53 -0700

    ast: add a `TypeSystem` pass which builds a BTF type system
    This includes only external types, but these can be reconciled for calls
    (at least in one direction), later on. Since this will be consumed by
    the semantic analyzer, it means that `ClangBuild` also becomes a
    required pass in most cases.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4339, branch: user/amscanne/btf_min2/2

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@
 #include "ast/passes/resource_analyser.h"
 #include "ast/passes/return_path_analyser.h"
 #include "ast/passes/semantic_analyser.h"
+#include "ast/passes/type_system.h"
 #include "benchmark.h"
 #include "bpffeature.h"
 #include "bpftrace.h"
@@ -330,6 +331,8 @@ void CreateDynamicPasses(std::function<void(ast::Pass&& pass)> add)
   add(ast::CreateFoldLiteralsPass());
   add(ast::CreatePidFilterPass());
   add(ast::CreateNamedParamsPass());
+  add(ast::CreateClangBuildPass());
+  add(ast::CreateTypeSystemPass());
   add(ast::CreateSemanticPass());
   add(ast::CreateResourcePass());
   add(ast::CreateRecursionCheckPass());
@@ -341,6 +344,8 @@ void CreateAotPasses(std::function<void(ast::Pass&& pass)> add)
   add(ast::CreatePortabilityPass());
   add(ast::CreateFoldLiteralsPass());
   add(ast::CreateNamedParamsPass());
+  add(ast::CreateClangBuildPass());
+  add(ast::CreateTypeSystemPass());
   add(ast::CreateSemanticPass());
   add(ast::CreateResourcePass());
   add(ast::CreateRecursionCheckPass());
@@ -780,6 +785,7 @@ int main(int argc, char* argv[])
     // probe. The raw contents of the program are the initial search provided.
     ast = buildListProgram(is_search_a_type ? FULL_SEARCH : args.search);
     ast::CDefinitions no_c_defs; // No external C definitions may be used.
+    ast::TypeMetadata no_types;  // No external types may be used.
 
     // Parse and expand all the attachpoints. We don't need to descend into
     // the actual driver here, since we know that the program is already
@@ -788,6 +794,7 @@ int main(int argc, char* argv[])
                         .put(ast)
                         .put(bpftrace)
                         .put(no_c_defs)
+                        .put(no_types)
                         .add(ast::CreateParseAttachpointsPass(args.listing))
                         .add(CreateParseBTFPass())
                         .add(ast::CreateMapSugarPass())
@@ -873,8 +880,10 @@ int main(int argc, char* argv[])
 
   if (args.listing) {
     ast::CDefinitions no_c_defs; // See above.
+    ast::TypeMetadata no_types;  // See above.
     pm.add(CreateParsePass())
         .put(no_c_defs)
+        .put(no_types)
         .add(ast::CreateParseAttachpointsPass(args.listing))
         .add(CreateParseBTFPass())
         .add(ast::CreateMapSugarPass())
@@ -907,6 +916,7 @@ int main(int argc, char* argv[])
   for (auto& pass : ast::AllParsePasses(std::move(flags))) {
     addPass(std::move(pass));
   }
+  pm.add(ast::CreateLLVMInitPass());
 
   switch (args.build_mode) {
     case BuildMode::DYNAMIC:
@@ -917,8 +927,9 @@ int main(int argc, char* argv[])
       break;
   }
 
-  pm.add(ast::CreateLLVMInitPass());
-  pm.add(ast::CreateClangBuildPass());
+  if (bt_debug.contains(DebugStage::Types)) {
+    pm.add(ast::CreateDumpTypesPass(std::cout));
+  }
   pm.add(ast::CreateCompilePass());
   pm.add(ast::CreateLinkBitcodePass());
   if (bt_debug.contains(DebugStage::Codegen)) {
