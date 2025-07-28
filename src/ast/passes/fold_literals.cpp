b92c4ff3a9c735f30081a15dd5a4f40a10837218
Adin Scannell <amscanne@meta.com>
2025-07-27 19:07:00 -0700

    typeof: introduce new operator
    To allow for type-specific behavior in macros and other parts of the
    standard library, we need a way to do parameteric behavior based on
    types. Rather than overloading, if we introduce a `typeof` operator and
    a type comparison expression, then we can simply prune invalid branches
    during semantic analysis. These branches can correspond to expanded
    macros or anything else.
    
    ```
    macro is_signed($x) {
      let $r;
      if (typeof($x) == typeof(0xffffffffffffffff)) {
        $r = false;
      } else {
        $r = true;
      }
      $r
    }
    
    begin {
      if (is_signed(-1)) {
        print("-1 number is signed")
      } else {
        print("-1 number isn't signed")
      }
      if (is_signed(0xffffffffffffffff)) {
        print("big number number is signed")
      } else {
        print("big number number isn't signed")
      }
    }
    ```
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4401, branch: user/amscanne/add_typeof/1

--- src/ast/passes/fold_literals.cpp
+++ src/ast/passes/fold_literals.cpp
@@ -631,6 +631,12 @@ std::optional<Expression> LiteralFolder::visit(Builtin &builtin)
   return std::nullopt;
 }
 
+void fold(ASTContext &ast, BPFtrace &b, Expression &expr)
+{
+  LiteralFolder folder(ast, b);
+  folder.visit(expr);
+}
+
 Pass CreateFoldLiteralsPass()
 {
   auto fn = [](ASTContext &ast, BPFtrace &b) {
