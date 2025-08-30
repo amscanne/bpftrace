779a839f5df071f0cfc3013f7c7bdc0b1cd84592
Adin Scannell <amscanne@meta.com>
2025-08-29 22:13:18 -0700

    Add kernel types to type system

--- src/ast/passes/type_system.cpp
+++ src/ast/passes/type_system.cpp
@@ -1,5 +1,9 @@
-#include "ast/passes/type_system.h"
+#include <fstream>
+#include <unordered_map>
+
+#include "ast/ast.h"
 #include "ast/passes/clang_build.h"
+#include "ast/passes/type_system.h"
 
 namespace bpftrace::ast {
 
@@ -8,30 +12,36 @@ Pass CreateTypeSystemPass()
   auto fn = [](BitcodeModules &bm) -> Result<TypeMetadata> {
     TypeMetadata result;
 
+    // Load all kernel types.
+    std::string kernel_btf_source = "/sys/kernel/btf/vmlinux";
+    std::ifstream kernel_btf(kernel_btf_source);
+    if (!kernel_btf.is_open()) {
+      return make_error<SystemError>("Unable to read " + kernel_btf_source);
+    }
+    auto kernel_btf_data = std::string(std::istreambuf_iterator<char>(kernel_btf),
+                                std::istreambuf_iterator<char>());
+    auto kernel_types = btf::Types::parse(static_cast<const void*>(kernel_btf_data.data()), kernel_btf_data.size());
+    if (!kernel_types) {
+      return kernel_types.takeError();
+    }
+    auto aggregate = std::move(*kernel_types); // Initial type set.
+
     // For now, we simply build a single type system that covers all the
     // external imports and standard library. In theory, this should be rebased
     // on top of the individual probe type system (coming from the kernel,
     // module or user binary).
-    std::optional<btf::Types> aggregate;
     for (const auto &s : bm.objects) {
       auto btf = btf::Types::parse(static_cast<const void *>(s.data()),
                                    s.size());
       if (!btf) {
         return btf.takeError();
       }
-      if (!aggregate) {
-        aggregate.emplace(std::move(*btf));
-      } else {
-        auto ok = aggregate->append(*btf);
-        if (!ok) {
-          return ok.takeError();
-        }
+      auto ok = aggregate.append(*btf);
+      if (!ok) {
+        return ok.takeError();
       }
     }
-    if (aggregate) {
-      result.global = std::move(*aggregate);
-    }
-
+    result.global = std::move(aggregate);
     return result;
   };
 
