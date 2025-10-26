eb4beaf9eea262012e6af6c3ea46b2009e8c790c
Adin Scannell <adin@scannell.ca>
2025-10-25 20:52:08 -0700

    types: ignore empty C files
    These currently fail to parse BTF, because there is none. In this case,
    associate the error with the node itself, and ignore if it indicates
    that there is no BTF for the object.
    
    Signed-off-by: Adin Scannell <adin@scannell.ca>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4776, branch: user/amscanne/user/amscanne/empty_types/1/1

--- src/ast/passes/type_system.cpp
+++ src/ast/passes/type_system.cpp
@@ -16,11 +16,27 @@ Pass CreateTypeSystemPass()
     // on top of the individual probe type system (coming from the kernel,
     // module or user binary).
     std::optional<btf::Types> aggregate;
-    for (const auto &s : bm.objects) {
+    for (size_t i = 0; i < bm.objects.size(); i++) {
+      const auto &node = bm.nodes[i].get();
+      const auto &s = bm.objects[i];
       auto btf = btf::Types::parse(static_cast<const void *>(s.data()),
                                    s.size());
       if (!btf) {
-        return btf.takeError();
+        // If we encounter some error parsing BTF, add it to the specific
+        // import directly. If there is no BTF (e.g. an empty C file), then
+        // we can just suppress the warning.
+        auto err = handleErrors(std::move(btf),
+                                [&node](const btf::ParseError &parse_err) {
+                                  if (parse_err.error_code() != ENODATA) {
+                                    node.addWarning()
+                                        << "Failed to parse BTF data: "
+                                        << strerror(parse_err.error_code());
+                                  }
+                                });
+        if (!err) {
+          return err.takeError();
+        }
+        continue; // Skip this file.
       }
       if (!aggregate) {
         aggregate.emplace(std::move(*btf));
