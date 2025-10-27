703a3dbe9b696c956c62c3bff21842b3586eaec3
Adin Scannell <adin@scannell.ca>
2025-10-26 22:18:21 -0700

    f

--- src/ast/passes/type_system.cpp
+++ src/ast/passes/type_system.cpp
@@ -1,14 +1,12 @@
-#include <unordered_map>
-
-#include "ast/ast.h"
-#include "ast/passes/clang_build.h"
 #include "ast/passes/type_system.h"
+#include "ast/context.h"
+#include "ast/passes/clang_build.h"
 
 namespace bpftrace::ast {
 
 Pass CreateTypeSystemPass()
 {
-  auto fn = [](BitcodeModules &bm) -> Result<TypeMetadata> {
+  auto fn = [](ASTContext &ast, BitcodeModules &bm) -> Result<TypeMetadata> {
     TypeMetadata result;
 
     // For now, we simply build a single type system that covers all the
@@ -16,21 +14,21 @@ Pass CreateTypeSystemPass()
     // on top of the individual probe type system (coming from the kernel,
     // module or user binary).
     std::optional<btf::Types> aggregate;
-    for (size_t i = 0; i < bm.objects.size(); i++) {
-      const auto &node = bm.nodes[i].get();
-      const auto &s = bm.objects[i];
-      auto btf = btf::Types::parse(static_cast<const void *>(s.data()),
-                                   s.size());
+    for (const auto &result : bm.modules) {
+      auto btf = btf::Types::parse(static_cast<const void *>(
+                                       result.object.data()),
+                                   result.object.size());
       if (!btf) {
         // If we encounter some error parsing BTF, add it to the specific
         // import directly. If there is no BTF (e.g. an empty C file), then
         // we can just suppress the warning.
         auto err = handleErrors(std::move(btf),
-                                [&node](const btf::ParseError &parse_err) {
+                                [&](const btf::ParseError &parse_err) {
                                   if (parse_err.error_code() != ENODATA) {
-                                    node.addWarning()
-                                        << "Failed to parse BTF data: "
-                                        << strerror(parse_err.error_code());
+                                    auto &diag = ast.diagnostics().addWarning(
+                                        result.loc);
+                                    diag << "Failed to parse BTF data: "
+                                         << strerror(parse_err.error_code());
                                   }
                                 });
         if (!err) {
