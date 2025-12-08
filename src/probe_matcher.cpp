d0393f63c92bc6cdaf0134a7acdfa61c81062ae2
Adin Scannell <amscanne@meta.com>
2025-12-07 21:51:14 -0800

    cleanup: refactor BTF and use natively for context resolution
    BTF has been available in all supported kernels for a long time, and
    many code paths are likely broken without BTF. This change simply
    requires BTF and documents how to provide external BTF if is it not
    directly available.
    
    This change is rather large because of the high level of interconnected
    classes and components in the system. This is substantially reduced by
    this change, and `BPFtrace&` is needed by a lot less internal plumbing.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4894, branch: user/amscanne/funcidx/3

--- src/probe_matcher.cpp
+++ src/probe_matcher.cpp
@@ -17,7 +17,6 @@
 #include "util/paths.h"
 #include "util/strings.h"
 #include "util/symbols.h"
-#include "util/system.h"
 #include "util/wildcard.h"
 
 #include <bcc/bcc_elf.h>
@@ -132,12 +131,6 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
       symbol_stream = std::move(*result);
       break;
     }
-    // The two `has_btf_data` checks below for fentry/fexit/rawtracepoints
-    // are more about ordering than system properties.
-    // Initially, before BTF is loaded and we want to determine what modules to
-    // load BTF from we check "/sys/kernel/tracing/available_filter_functions".
-    // Then we check the BTF to filter out functions from that list that don't
-    // have any BTF definitions.
     case ProbeType::rawtracepoint: {
       symbol_stream = get_raw_tracepoint_symbols();
       break;
@@ -168,9 +161,6 @@ std::set<std::string> ProbeMatcher::get_matches_for_probetype(
       break;
     }
     case ProbeType::iter: {
-      if (!bpftrace_->has_btf_data())
-        break;
-
       std::string ret;
       auto iters = bpftrace_->btf_->get_all_iters();
       for (const auto& iter : iters) {
@@ -246,9 +236,7 @@ std::unique_ptr<std::istream> ProbeMatcher::get_symbols_from_traceable_funcs(
 std::unique_ptr<std::istream> ProbeMatcher::get_fentry_symbols(
     const std::string& mod) const
 {
-  if (bpftrace_->btf_->has_data() && bpftrace_->btf_->modules_loaded()) {
-    return bpftrace_->btf_->get_all_funcs();
-  } else if (!mod.empty() && !util::has_wildcard(mod)) {
+  if (!mod.empty() && !util::has_wildcard(mod)) {
     return get_symbols_from_traceable_funcs(true, mod);
   } else {
     return get_symbols_from_traceable_funcs(true);
@@ -267,7 +255,7 @@ std::unique_ptr<std::istream> ProbeMatcher::get_running_bpf_programs() const
 
 std::unique_ptr<std::istream> ProbeMatcher::get_raw_tracepoint_symbols() const
 {
-  if (bpftrace_->btf_->has_data() && bpftrace_->btf_->modules_loaded()) {
+  if (bpftrace_->btf_->objects_cnt() > 1) {
     return bpftrace_->btf_->get_all_raw_tracepoints();
   } else {
     std::string rts;
